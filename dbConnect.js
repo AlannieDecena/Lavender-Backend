d9ce90158e76534b5c7a1a644f9c29b6677f8198
alanniedecena <alannie.khem.decena@gmail.com>
2023-07-29 14:25:23 +1200

    chcange dialect name

--- dbConnect.js
+++ dbConnect.js
@@ -11,7 +11,7 @@ const { Sequelize } = require('sequelize');
 // Create a new instance of Sequelize with the database credentials
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    dialect: 'mysql'
+    dialect: 'postgres',
 });
 
 // Log the database user to the console
@@ -23,7 +23,7 @@ const connectMysql = async () => {
         await sequelize.authenticate();
         console.log(`Successful connection to MySQL Database ${process.env.DB_NAME}`);
     } catch (error) {
-        console.error('Unable to connect to MySQL database:', error);
+        console.error(`Unable to connect to MySQL database:${process.env.DB_NAME}`, error);
         process.exit(1);
     }
 }
